c233303bce16cbf64ee72c089d4518e81cdadbfe
tayyaba <tayyabashahbaz2020@gmail.com>
2024-11-24 23:45:21 +0500

    img tag change

--- grid/src/app/page.tsx
+++ grid/src/app/page.tsx
@@ -1,7 +1,8 @@
+import Image from "next/image"
 export default function Home(){
   return(
     <div>
-      <img className="w-full h-full sm:h-96 md:h-[500px] lg:h-[600px]" src="home.jpg" alt="home cover picture" />
+      <Image className="w-full h-full sm:h-96 md:h-[500px] lg:h-[600px]" src="home.jpg" alt="home cover picture" />
     </div>
   )
 }
