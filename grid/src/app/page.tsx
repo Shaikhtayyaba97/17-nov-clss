fa0de05157646109417d0a915e4cb5a98f34ab17
tayyaba <tayyabashahbaz2020@gmail.com>
2024-11-25 23:25:35 +0500

    little changes

--- grid/src/app/page.tsx
+++ grid/src/app/page.tsx
@@ -1,8 +1,10 @@
+
+
 import Image from "next/image"
 export default function Home(){
   return(
-    <div>
-      <Image className="w-full h-full sm:h-96 md:h-[500px] lg:h-[600px]" src="home.jpg" alt="home cover picture" />
+    <div >
+      <Image className="w-full h-full sm:h-96 md:h-[500px]  lg:h-[600px]" src="/home.jpg" alt="home cover picture fill"   width={500} height={500}  />
     </div>
-  )
+  );
 }
